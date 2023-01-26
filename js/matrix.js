55555401614cd239cdcb2464b7bb86397f2b9bce
lucasrenandns <lucasrenandns96@gmail.com>
2023-01-26 17:11:08 -0300

    Matrix Effect

---
+++ js/matrix.js
@@ -0,0 +1,49 @@
+// Selecionando os elementos
+const section = document.getElementById("section")
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d")
+
+// Definindo o tamanho da seção
+canvas.width = section.offsetWidth
+canvas.height = section.offsetHeight
+const sectionWidth = canvas.width
+const sectionHeight = canvas.height
+
+// Variáveis para a criação do efeito
+const katakana = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン"
+const latim = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789"
+const alphabet = katakana + latim + numbers
+const fontSize = 16
+const columns = Math.floor(sectionWidth / fontSize)
+const drops = []
+
+// Laço para dividir as colunas da seção
+for(let x = 0; x < columns; x++) {
+    drops[x] = 1
+}
+
+// Função para criar o estilo do efeito
+function matrixEffectStyle() {
+    context.fillStyle = "rgba(0, 0, 0, 0.05)"
+    context.fillRect(0, 0, sectionWidth, sectionHeight)
+    context.fillStyle = "#0f0"
+    context.font = fontSize + "px sans-serif"
+}
+
+// Função para criar o efeito
+function matrixEffect() {
+    for(let i = 0; i < drops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+        context.fillText(text, i * fontSize, drops[i] * fontSize)
+
+        if(drops[i] * fontSize > sectionHeight && Math.random() > 0.975) {
+            drops[i] = 0
+        }
+        drops[i]++
+    }
+
+    matrixEffectStyle()
+}
+// Adicionando a função matrix effect dentro de um intervalo
+setInterval(matrixEffect, 30);
---
+++ js/typed.js
@@ -0,0 +1,8 @@
+// Efeito de digitação
+const typed = new Typed("#typed", {
+    strings: ["Hello, be welcome!", "My name is Lucas Renan", "I'm 26 years old", "I'm from Brazil, SP",
+    "I study Information Technology at Univesp", "This is", "Matrix Effect ;D"],
+    typeSpeed: 40,
+    backSpeed: 30,
+    loop: true
+})
